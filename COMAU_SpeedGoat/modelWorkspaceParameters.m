3343b79f17310ebe6b285465b12bbd991c95607b
Thomas Lønne Stiansen <thomal2002@hotmail.com>
2025-06-11 12:51:16 +0200

    [WIP] Multibody model. Also added svg schematic by Tørdal for further updates and sg_IK from yesterday

--- COMAU_SpeedGoat/modelWorkspaceParameters.m
+++ COMAU_SpeedGoat/modelWorkspaceParameters.m
@@ -8,27 +8,27 @@ L3 = 1160e-3;
 L4 = 250e-3;
 L5 = 1492.18e-3;
 L6 = 210e-3; % Last length plus End-effector/hook
-L7 = 567e-3; %winch sheeve extension 567e-3
+L7 = 567e-3; % Winch sheave extension 567e-3
 
 
-%Zero configuration matrix
+% Zero configuration matrix
 M = [1 0 0 L1+L5+L6+L7;
      0 1 0 0;
      0 0 1 L2+L3+L4;
      0 0 0 1];
 
-%thetaList0 = [0 0 -1.57 0 0];
+% thetaList0 = [0 0 -1.57 0 0];
 
-%Defining Screw axis
+% Defining Screw axis
 
-%omega direction of screw axis
+% omega direction of screw axis
 omega1 = [0 0 -1]';
 omega2 = [0 1 0]';
 omega3 = [0 -1 0]';
 omega4 = [0 1 0]';
 omega5 = [0 0 1]';
 
-%q some arbitrary point along the screw axis
+% q some arbitrary point along the screw axis
 q1 = [0 0 0]';
 q2 = [L1 0 L2]';
 q3 = [L1 0 L2+L3]';
@@ -37,15 +37,15 @@ q5 = [L1+L5+L6+L7 0 L2+L3+L4]';
 
 omega_mat = [omega1 omega2 omega3 omega4 omega5];
 
-%The negative kinematic moment of the screw axis (v) at an arbitrary point (q)
-% -omega X q
+% The negative kinematic moment of the screw axis (v) at an arbitrary point (q)
+    % - omega \cross q
 v_mat = [-cross(omega1,q1) -cross(omega2,q2) -cross(omega3,q3)...
          -cross(omega4,q4) -cross(omega5,q5)];
 
 SList = [omega_mat; v_mat];
 % Same as [S1, S2, S3, S4, S5] - TLS
 
-%For body FK IK and jacobian
+% For body FK IK and jacobian
 M_B = TransInv(M);
 BList = Adjoint(M_B) * SList;
 
